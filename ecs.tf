bde46bfc0415710ee9d0fc74f6cb786fecb90bdd
VeerSavrakar <139918550+VeerSavrakar@users.noreply.github.com>
2024-09-04 11:52:37 +0530

    Add files via upload

---
+++ ecs.tf
@@ -0,0 +1,62 @@
+# resource "aws_ecs_cluster" "medusa_cluster" {
+#   name = "medusa-cluster"
+# }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRoleNew"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa_task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "medusa-backend"
+      image     = "851725444116.dkr.ecr.ap-south-1.amazonaws.com/medusa-backend:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 9000
+          hostPort      = 9000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name = "medusa_ser"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.subnet.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
